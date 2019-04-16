59ad23c6cc929716f17c6151b05548ff2267cd52
Sammy Cherna <cherna@mit.edu>
2019-04-15 23:46:29 -0400

    fixed variables

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ import './grammar.js';
 function makeVarContext() {
     let vars = UI.getVariables();
     let tfvars = vars.map((v, i, a) => ({match: v, tfvar: makeTfVar(v)}));
-    let x = {1:2};
-
     return Object.assign(...tfvars.map(
-            (v, i, a) => {
-                let k = {};
-                k[v.match.name] = v;
-                return k;
-            }
-        ));
+        (v, i, a) => {
+            let k = {};
+            k[v.match.name] = v;
+            return k;
+        }
+    ));
 }
 
-function getVariable(varName, varContext) {
+function getVariable(varName, varContext, usedVars) {
     let c = varContext[varName];
-    return {match: c.match, tfvar: c.tfvar};
+    usedVars[varName] = c;
+    return c;
 }
 
 function makeTfVar(v) {
@@ -41,21 +40,21 @@ function makeTfNumber(v) {
 if(true) {
 UI.setVisualizerStartHandler(() => {
     let varContext = makeVarContext();
+    let usedVars = {};
     let tokens = Array.from(moo.compile({
         WS: /[ \t]+/,
-        variable: {match: /[a-zA-Z]/, value: v => getVariable(v, varContext)},
+        variable: {match: /[a-zA-Z]/, value: v => getVariable(v, varContext, usedVars)},
         plus: /\+/,
         times: /\*/,
         normsign: /\|\|/,
         number: {match: /[1-9][0-9]*/, value: v => makeTfNumber(v)},
     }).reset(UI.getExpression())).filter((v, i, a) => v.type !== "WS");
     console.log(tokens);
-    let tfvars = tokens.filter((v, i, a) => v.type === "variable" && v.value.match.trainable === true).map((v, i, a) => v.value.tfvar);
+    let tfvars = Object.keys(usedVars).filter((v, i, a) => usedVars[v].match).map((v, i, a) => usedVars[v].tfvar);
     console.log(tfvars);
     const parser = new nearly.Parser(nearly.Grammar.fromCompiled(grammar));
     parser.feed(tokens);
     const f = parser.results[0];
-    console.log(f);
     generateLossSurfaceFromUI(tfvars, f, UI.getSettings());
 });
 }
